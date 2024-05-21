3de9b4ba776b6cc6eafb9589d698d5a189a657f0
Thien Hoang <thien.hoang@sapotacorp.vn>
2024-05-21 17:00:57 +0700

    Update image

--- src/pages/user/Components/Footer.tsx
+++ src/pages/user/Components/Footer.tsx
@@ -1,42 +1,41 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from "react";
+import { Link } from "react-router-dom";
 
 // IMAGES LOGO
-import logo2 from "../img/logo2.png"
+import logo2 from "../img/logo2.png";
 
 // IMAGES PAYMENTS
-import pay_1 from "../img/payment/payment-1.png"
-import pay_2 from "../img/payment/payment-2.png"
-import pay_3 from "../img/payment/payment-3.png"
-import pay_4 from "../img/payment/payment-4.png"
-import pay_5 from "../img/payment/payment-5.png"
-import './Footer.css';
+import pay_1 from "../img/payment/payment-1.png";
+import pay_2 from "../img/payment/payment-2.png";
+import pay_3 from "../img/payment/payment-3.png";
+import pay_4 from "../img/payment/payment-4.png";
+import pay_5 from "../img/payment/payment-5.png";
+import "./Footer.css";
 import { Typography } from "@material-tailwind/react";
 
-
-type Props = {}
+type Props = {};
 const LINKS = [
-    {
-      title: "Product",
-      items: ["Overview", "Features", "Solutions", "Tutorials"],
-    },
-    {
-      title: "Company",
-      items: ["About us", "Careers", "Press", "News"],
-    },
-    {
-      title: "Resource",
-      items: ["Blog", "Newsletter", "Events", "Help center"],
-    },
-  ];
-  const currentYear = new Date().getFullYear();
+  {
+    title: "Product",
+    items: ["Overview", "Features", "Solutions", "Tutorials"],
+  },
+  {
+    title: "Company",
+    items: ["About us", "Careers", "Press", "News"],
+  },
+  {
+    title: "Resource",
+    items: ["Blog", "Newsletter", "Events", "Help center"],
+  },
+];
+const currentYear = new Date().getFullYear();
 
 const Footer_component = (props: Props) => {
-    return (
-        <>
-            {/* FOOTER SESSION START */}
-            <div className="footer">
-            {/* <div className="footer-static-top">
+  return (
+    <>
+      {/* FOOTER SESSION START */}
+      <div className="footer">
+        {/* <div className="footer-static-top">
                 <div className="container">
                     <div className="footer-shipping pt-60 pb-55 pb-xs-25">
                         <div className="row">
@@ -88,74 +87,109 @@ const Footer_component = (props: Props) => {
                     </div>
                 </div>
             </div> */}
-            <div className="footer-static-middle">
-                <div className="container">
-                    <div className="footer-logo-wrap pt-50 pb-35">
-                        <div className="row">
-                            <div className="col-lg-4 col-md-6">
-                                <div className="footer-logo">
-                                   
-                                    <p className="info">
-                                        We are a team of designers and developers that create high quality HTML Template & Woocommerce, Shopify Theme.
-                                </p>
-                                </div>
-                                <ul className="des">
-                                    <li>
-                                        <span>Address: </span>
-                                        Khu đô thị FPT City, Ngũ Hành Sơn, Đà Nẵng 550000
-                                </li>
-                                    <li>
-                                        <span>Phone: </span>
-                                        <a href="#">0905222529</a>
-                                    </li>
-                                    <li>
-                                        <span>Email: </span>
-                                        <a href="mailto://tienkim9920@gmail.com">rjotnotinh16969@gmail.com</a>
-                                    </li>
-                                </ul>
-                            </div>
-                                <div className="col-lg-2 col-md-3 col-sm-6">
-                                    <div className="footer-block">
-                                        <h3 className="footer-block-title">Product</h3>
-                                        <ul>
-                                            <li><a href="#">Prices drop</a></li>
-                                            <li><a href="#">New products</a></li>
-                                            <li><a href="#">Best sales</a></li>
-                                            <li><a href="#">Contact us</a></li>
-                                        </ul>
-                                    </div>
-                                </div>
-                            <div className="col-lg-2 col-md-3 col-sm-6">
-                                <div className="footer-block">
-                                    <h3 className="footer-block-title">Our company</h3>
-                                        <ul>
-                                            <li><a href="#">Delivery</a></li>
-                                            <li><a href="#">Legal Notice</a></li>
-                                            <li><a href="#">About us</a></li>
-                                            <li><a href="#">Contact us</a></li>
-                                      </ul>
-                                </div>
-                            </div>
-                            <div className="col-lg-4">  
-                                <div className="footer-newsletter">
-                                    <h4>Sign up to newsletter</h4>
-                                    <form action="#" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" className="footer-subscribe-form validate" target="_blank">
-                                        <div id="mc_embed_signup_scroll">
-                                            <div id="mc-form" className="mc-form subscribe-form form-group" >
-                                                <input id="mc-email" type="email" autoComplete="off" placeholder="Enter your email" />
-                                                <button className="btn" id="mc-submit">Subscribe</button>
-                                            </div>
-                                        </div>
-                                    </form>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
+        <div className="footer-static-middle">
+          <div className="container">
+            <div className="footer-logo-wrap pt-50 pb-35">
+              <div className="row">
+                <div className="col-lg-4 col-md-6">
+                  <div className="footer-logo">
+                    <p className="info">
+                      We are a team of designers and developers that create high
+                      quality HTML Template & Woocommerce, Shopify Theme.
+                    </p>
+                  </div>
+                  <ul className="des">
+                    <li>
+                      <span>Address: </span>
+                      Khu đô thị FPT City, Ngũ Hành Sơn, Đà Nẵng 550000
+                    </li>
+                    <li>
+                      <span>Phone: </span>
+                      <a href="#">0905222529</a>
+                    </li>
+                    <li>
+                      <span>Email: </span>
+                      <a href="mailto://rjotnotinh16969@gmail.com">
+                        thienhn2k2@gmail.com
+                      </a>
+                    </li>
+                  </ul>
                 </div>
+                <div className="col-lg-2 col-md-3 col-sm-6">
+                  <div className="footer-block">
+                    <h3 className="footer-block-title">Product</h3>
+                    <ul>
+                      <li>
+                        <a href="#">Prices drop</a>
+                      </li>
+                      <li>
+                        <a href="#">New products</a>
+                      </li>
+                      <li>
+                        <a href="#">Best sales</a>
+                      </li>
+                      <li>
+                        <a href="#">Contact us</a>
+                      </li>
+                    </ul>
+                  </div>
+                </div>
+                <div className="col-lg-2 col-md-3 col-sm-6">
+                  <div className="footer-block">
+                    <h3 className="footer-block-title">Our company</h3>
+                    <ul>
+                      <li>
+                        <a href="#">Delivery</a>
+                      </li>
+                      <li>
+                        <a href="#">Legal Notice</a>
+                      </li>
+                      <li>
+                        <a href="#">About us</a>
+                      </li>
+                      <li>
+                        <a href="#">Contact us</a>
+                      </li>
+                    </ul>
+                  </div>
+                </div>
+                <div className="col-lg-4">
+                  <div className="footer-newsletter">
+                    <h4>Sign up to newsletter</h4>
+                    <form
+                      action="#"
+                      method="post"
+                      id="mc-embedded-subscribe-form"
+                      name="mc-embedded-subscribe-form"
+                      className="footer-subscribe-form validate"
+                      target="_blank"
+                    >
+                      <div id="mc_embed_signup_scroll">
+                        <div
+                          id="mc-form"
+                          className="mc-form subscribe-form form-group"
+                        >
+                          <input
+                            id="mc-email"
+                            type="email"
+                            autoComplete="off"
+                            placeholder="Enter your email"
+                          />
+                          <button className="btn" id="mc-submit">
+                            Subscribe
+                          </button>
+                        </div>
+                      </div>
+                    </form>
+                  </div>
+                </div>
+              </div>
             </div>
+          </div>
         </div>
-        </>
-    )
-}
+      </div>
+    </>
+  );
+};
 
-export default Footer_component
+export default Footer_component;
